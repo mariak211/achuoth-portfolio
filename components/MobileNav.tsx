e0f1a24966995e55f91aca3a6fb370e4553d972e
Mariak Achuoth <achuothmariak@gmail.com>
2024-09-07 22:48:44 -0400

    add automatic dismisal of mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { CiMenuFries } from "react-icons/ci";
@@ -8,20 +9,29 @@ import { Button } from "./ui/button";
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setOpen(false);
+  };
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
 
+      {/* Make the sheet content scrollable */}
       <SheetContent className="flex flex-col max-h-[80vh] overflow-y-auto">
-        <div className=" mt-32 mb-40 text-center text-2xl">
-          <Link href="/">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/" onClick={handleLinkClick}>
             <h1 className="text-sm font-semibold border border-green-400 rounded-xl p-4">
               Achuoth<span className="text-accent">.</span>Mariak
             </h1>
           </Link>
         </div>
+
+        {/* Navigation links */}
         <nav className="flex flex-col justify-center items-center gap-8">
           {nav_links.map((link, index) => {
             return (
@@ -33,13 +43,15 @@ const MobileNav = () => {
                     ? "text-accent border-b-2 border-accent"
                     : ""
                 }`}
+                onClick={handleLinkClick}
               >
                 {link.name}
               </Link>
             );
           })}
+
           <div className="flex items-center gap-8">
-            <Link href="/contact">
+            <Link href="/contact" onClick={handleLinkClick}>
               <Button>Hire Me</Button>
             </Link>
           </div>
